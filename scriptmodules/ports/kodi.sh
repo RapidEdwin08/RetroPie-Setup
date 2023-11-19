21ae7e12c4b437873dce3e74ca6965ebb7556523
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-11-19 01:58:43 -0500

    Bookworm

--- scriptmodules/ports/kodi.sh
+++ scriptmodules/ports/kodi.sh
@@ -59,7 +59,7 @@ function install_bin_kodi() {
 
     # not all the kodi packages may be available depending on repository
     # so we will check and install what's available
-    local all_pkgs=(kodi kodi-peripheral-joystick kodi-inputstream-adaptive kodi-vfs-libarchive kodi-vfs-sftp kodi-vfs-nfs)
+    local all_pkgs=(kodi kodi-peripheral-joystick kodi-inputstream-adaptive kodi-vfs-libarchive kodi-vfs-sftp)
     compareVersions "$__os_ubuntu_ver" lt 22.04 && all_pkgs+=(kodi-inputstream-rtmp)
     local avail_pkgs=()
     local pkg
