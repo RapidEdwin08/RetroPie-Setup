4827fde3d7496eabfd15762e2c28e2b4ecc409d1
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2024-01-26 12:30:35 -0500

    Update aethersx2.sh

--- ext/RetroPie-Extra/scriptmodules/emulators/aethersx2.sh
+++ ext/RetroPie-Extra/scriptmodules/emulators/aethersx2.sh
@@ -10,7 +10,7 @@
 #
 
 rp_module_id="aethersx2"
-rp_module_desc="PS2 Emulator Optimized for ARM                                                                                                  \n \n AetherSX2-v1.5-3606.AppImage \n \n https://www.aethersx2.com/archive/?dir=desktop/linux \n \n AetherSX2 uses third-party code. You can view the licenses for this code by selecting \"Third-Party Notices\" in the navigation menu of the app (swipe out from the left)."
+rp_module_desc="PS2 Emulator Optimized for ARM                                                                                                                                                                               \n \n AetherSX2-v1.5-3606.AppImage \n \n https://www.aethersx2.com/archive/?dir=desktop/linux \n \n AetherSX2 uses third-party code. \n You can view the licenses for this code by selecting \"Third-Party Notices\""
 rp_module_licence="Aethersx2 https://aethersx2.net/terms-conditions"
 rp_module_section="exp"
 rp_module_flags="!all arm aarch64 rpi4 rpi5"
@@ -27,23 +27,23 @@ function sources_aethersx2() {
 function install_aethersx2() {
     chmod 755 "AetherSX2-v1.5-3606.AppImage"; mv 'AetherSX2-v1.5-3606.AppImage' "$md_inst"
     chmod 755 "aethersx2.sh"; mv "aethersx2.sh" "$md_inst"
-    chmod 755 "AetherSX2.desktop"; mv "AetherSX2.desktop" "$md_inst"
-    mv "AetherSX2-128.png" "$md_inst"; mv "PS2BIOSRequired.jpg" "$md_inst"
+    chmod 755 "AetherSX2.desktop"; cp "AetherSX2.desktop" "$md_inst"
+    mv "AetherSX2.desktop" "$home/Desktop"; chown "$(basename $home)" "$home/Desktop/AetherSX2.desktop"
+    mv "AetherSX2-128.xpm" "$md_inst"; mv "PS2BIOSRequired.jpg" "$md_inst"
 
     if [[ ! -d "$home/.config/aethersx2" ]]; then mkdir "$home/.config/aethersx2"; fi
-	if [[ ! -d "$home/.config/aethersx2/inis" ]]; then mkdir "$home/.config/aethersx2/inis"; fi
+    if [[ ! -d "$home/.config/aethersx2/inis" ]]; then mkdir "$home/.config/aethersx2/inis"; fi
     if [[ ! -f "$home/.config/aethersx2/inis/PCSX2.ini" ]]; then mv "PCSX2.ini" "$home/.config/aethersx2/inis"; fi
-	if [[ ! -d "$home/.config/aethersx2/bios" ]]; then ln -s /home/pi/RetroPie/BIOS "$home/.config/aethersx2/bios"; fi
+    if [[ ! -d "$home/.config/aethersx2/bios" ]]; then ln -s /home/pi/RetroPie/BIOS "$home/.config/aethersx2/bios"; fi
+    if [[ ! -f "$home/RetroPie/BIOS/Mcd001.ps2" ]]; then mv "Mcd001.ps2" "$home/RetroPie/BIOS"; fi
+    if [[ ! -f "$home/RetroPie/BIOS/Mcd002.ps2" ]]; then mv "Mcd002.ps2" "$home/RetroPie/BIOS"; fi
     chown "$(basename $home)" -R "$home/.config/aethersx2"
 
     mkdir "$home/RetroPie/roms/ps2"
-    mv '+Start AetherSX2.sh' "$home/RetroPie/roms/ps2"
+    chmod 755 '+Start AetherSX2.sh'; mv '+Start AetherSX2.sh' "$home/RetroPie/roms/ps2"
     if [[ ! -f "$home/RetroPie/roms/ps2/gamelist.xml" ]]; then mv 'gamelist.xml' "$home/RetroPie/roms/ps2"; fi
     if [[ ! -d "$home/RetroPie/roms/ps2/media" ]]; then mv 'media' "$home/RetroPie/roms/ps2"; fi
     chown "$(basename $home)" -R "$home/RetroPie/roms/ps2"
-
-    if [[ ! -f "$home/Desktop/AetherSX2.desktop" ]]; then cp "/opt/retropie/emulators/aethersx2/AetherSX2.desktop" "$home/Desktop"; fi
-    chown "$(basename $home)" "$home/Desktop/AetherSX2.desktop"
 }
 
 function configure_aethersx2() {
