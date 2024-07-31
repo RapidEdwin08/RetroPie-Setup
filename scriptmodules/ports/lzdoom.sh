5581240d7e826902d2762ae65174e94c157f2f49
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2024-07-31 16:33:13 -0400

    Update lzdoom.sh

--- scriptmodules/ports/lzdoom.sh
+++ scriptmodules/ports/lzdoom.sh
@@ -32,6 +32,7 @@ function sources_lzdoom() {
         applyPatch "$md_data/01_remove_cmake_arm_options.diff"
         # patch the 21.06 version of LZMA-SDK to disable the CRC32 ARMv8 intrinsics forced for ARM CPUs
         applyPatch "$md_data/02_lzma_sdk_dont_force_arm_crc32.diff"
+    fi
 }
 
 function build_lzdoom() {
