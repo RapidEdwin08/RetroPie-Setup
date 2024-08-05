6986bf6b8616f14968862750b1ee9efca6164565
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2024-08-04 20:54:56 -0400

    Update dsda-doom.sh

--- scriptmodules/ports/dsda-doom.sh
+++ scriptmodules/ports/dsda-doom.sh
@@ -17,9 +17,9 @@ rp_module_section="exp"
 rp_module_flags="sdl2"
 
 function depends_dsda-doom() {
-    local depends=(cmake libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libogg-dev libmad0-dev libvorbis-dev libzip-dev zlib1g-dev zipcmp zipmerge ziptool)
+    local depends=(cmake libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libogg-dev libmad0-dev libvorbis-dev libzip-dev zlib1g-dev zipcmp zipmerge ziptool libfluidsynth3 libfluidsynth-dev)
     # we need Fluidsynth 2+, check whether the platform has the older libfluidsynth1
-    [[ -z "$(dpkg-query -W -f '${Version}' libfluidsynth3)" ]] && depends+=(libfluidsynth-dev)
+    #[[ -z "$(dpkg-query -W -f '${Version}' libfluidsynth1)" ]] && depends+=(libfluidsynth-dev)
 
     getDepends "${depends[@]}"
 }
