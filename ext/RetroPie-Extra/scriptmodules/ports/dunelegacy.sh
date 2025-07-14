7bc360bbf1f7174133d54313dd2f5fe4753450b0
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-07-13 23:16:16 -0400

    Update dunelegacy.sh

--- ext/RetroPie-Extra/scriptmodules/ports/dunelegacy.sh
+++ ext/RetroPie-Extra/scriptmodules/ports/dunelegacy.sh
@@ -20,8 +20,11 @@ rp_module_section="exp"
 rp_module_flags="!mali"
 
 function depends_dunelegacy() {
-    #getDepends autotools-dev libsdl2-mixer-dev libopusfile0 libsdl2-mixer-2.0-0 libsdl2-ttf-dev xorg matchbox-window-manager x11-xserver-utils  libfluidsynth-dev  libfluidsynth1 fluidsynth
-	getDepends autotools-dev libsdl2-mixer-dev libopusfile0 libsdl2-mixer-2.0-0 libsdl2-ttf-dev xorg matchbox-window-manager x11-xserver-utils libfluidsynth-dev libfluidsynth3 fluidsynth
+    if [[ $(apt-cache search libfluidsynth3) == '' ]]; then
+		getDepends autotools-dev libsdl2-mixer-dev libopusfile0 libsdl2-mixer-2.0-0 libsdl2-ttf-dev xorg matchbox-window-manager x11-xserver-utils libfluidsynth-dev libfluidsynth1 fluidsynth
+	else
+		getDepends autotools-dev libsdl2-mixer-dev libopusfile0 libsdl2-mixer-2.0-0 libsdl2-ttf-dev xorg matchbox-window-manager x11-xserver-utils libfluidsynth-dev libfluidsynth3 fluidsynth
+	fi
 }
 
 function sources_dunelegacy() {
@@ -29,22 +32,27 @@ function sources_dunelegacy() {
 }
 
 function build_dunelegacy() {
-    ##params=(--prefix="$md_inst")
-    params=(--with-asound --without-pulse --with-sdl2 --prefix="$md_inst")
+    if [[ "$__os_debian_ver" -le 10 ]]; then
+		params=(--prefix="$md_inst")
+	else
+		params=(--prefix="$md_inst" --with-asound --without-pulse --with-sdl2)
+	fi
 	sed -i "/*Mix_Init(MIX_INIT_FLUIDSYNTH | MIX_INIT_FLAC | MIX_INIT_MP3 | MIX_INIT_OGG)/c\\Mix_Init(MIX_INIT_MID | MIX_INIT_FLAC | MIX_INIT_MP3 | MIX_INIT_OGG)" $md_build/src/FileClasses/music/DirectoryPlayer.cpp
 	sed -i "/if((Mix_Init(MIX_INIT_FLUIDSYNTH) & MIX_INIT_FLUIDSYNTH) == 0) {/c\\if((Mix_Init(MIX_INIT_MID) & MIX_INIT_MID) == 0) {" $md_build/src/FileClasses/music/XMIPlayer.cpp
 	
-	# Bookworm error: field ‘mentatStrings’ has incomplete type ‘std::array<std::unique_ptr<MentatTextFile>, 3>’
-	sed -i '1s/^/#include <tuple> \n/' $md_build/src/FileClasses/TextManager.cpp
-	sed -i '1s/^/#include <array> \n/' $md_build/src/FileClasses/TextManager.cpp
+	if [[ "$__os_debian_ver" -ge 11 ]]; then
+		# Bookworm error: field ‘mentatStrings’ has incomplete type ‘std::array<std::unique_ptr<MentatTextFile>, 3>’
+		sed -i '1s/^/#include <tuple> \n/' $md_build/src/FileClasses/TextManager.cpp
+		sed -i '1s/^/#include <array> \n/' $md_build/src/FileClasses/TextManager.cpp
+		
+		# Flickering fix -> Comment 1st instance of  // SDL_RenderPresent(renderer);
+		##sed -i 's+SDL_RenderPresent(renderer);+//SDL_RenderPresent(renderer);+g' $md_build/src/Game.cpp
+		sed '0,/SDL_RenderPresent(renderer);/s//\/\/SDL_RenderPresent(renderer);/' $md_build/src/Game.cpp > /dev/shm/Game.cpp; sudo mv /dev/shm/Game.cpp $md_build/src/Game.cpp
+	fi
 	
-	# Flickering fix -> Comment 1st instance of  // SDL_RenderPresent(renderer);
-	##sed -i 's+SDL_RenderPresent(renderer);+//SDL_RenderPresent(renderer);+g' $md_build/src/Game.cpp
-	sed '0,/SDL_RenderPresent(renderer);/s//\/\/SDL_RenderPresent(renderer);/' $md_build/src/Game.cpp > /dev/shm/Game.cpp; sudo mv /dev/shm/Game.cpp $md_build/src/Game.cpp
-	
+	echo [PARAMS]: ${params[@]}
     autoreconf --install
     ./configure "${params[@]}"
-    ##make 
     make -j4
 	md_ret_require="$md_build/src/dunelegacy"
 }
@@ -55,11 +63,12 @@ function install_dunelegacy() {
 
 function game_data_dunelegacy() {
     if [[ ! -f "$romdir/ports/dune2/data/DUNE2.EXE" ]]; then
-        downloadAndExtract "https://github.com/Exarkuniv/game-data/raw/main/dune-II.zip" "$romdir/ports/dune2"
-    mv "$romdir/ports/dune2/dune-ii-the-building-of-a-dynasty/"* "$romdir/ports/dune2/data"
-    rmdir "$romdir/ports/dune2/dune-ii-the-building-of-a-dynasty/"
-    chown -R $user:$user "$romdir/ports/dune2"
-    fi
+		if [[ ! -d "$romdir/ports/dune2/data" ]]; then mkdir "$romdir/ports/dune2/data"; fi
+		downloadAndExtract "https://github.com/Exarkuniv/game-data/raw/main/dune-II.zip" "$romdir/ports/dune2"
+		mv "$romdir/ports/dune2/dune-ii-the-building-of-a-dynasty/"* "$romdir/ports/dune2/data"
+		rmdir "$romdir/ports/dune2/dune-ii-the-building-of-a-dynasty/"
+		chown -R $user:$user "$romdir/ports/dune2"
+	fi
 }
 
 function configure_dunelegacy() {
