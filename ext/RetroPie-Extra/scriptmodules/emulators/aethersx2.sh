89a56891ac991122b3d51d556fecfb7b460a0b8a
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-07-16 07:54:07 -0400

    Add files via upload

--- ext/RetroPie-Extra/scriptmodules/emulators/aethersx2.sh
+++ ext/RetroPie-Extra/scriptmodules/emulators/aethersx2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is NOT part of The RetroPie Project
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -8,12 +8,15 @@
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
+#
+# If no user is specified (for RetroPie below v4.8.9)
+if [[ -z "$__user" ]]; then __user="$SUDO_USER"; [[ -z "$__user" ]] && __user="$(id -un)"; fi
 
 rp_module_id="aethersx2"
 rp_module_desc="PS2 Emulator Optimized for ARM                                                                                                                                                                                                     \n \n AetherSX2-v1.5-3606.AppImage \n \n http://web.archive.org/web/20240222085515/https://www.aethersx2.com/archive/?dir=desktop/linux \n \n AetherSX2 uses third-party code. \n You can view the licenses for this code by selecting \"Third-Party Notices\" \n \n RPi5 may need [kernel=kernel8.img] in [boot/config.txt]"
 rp_module_licence="Aethersx2 https://aethersx2.net/terms-conditions"
 rp_module_section="exp"
-rp_module_flags="!all arm aarch64 rpi4 rpi5"
+rp_module_flags="!all arm aarch64 rpi4 rpi5 !x86"
 
 function depends_aethersx2() {
     getDepends libfuse2 mesa-vulkan-drivers libvulkan-dev libsdl2-dev matchbox
@@ -30,7 +33,7 @@ function install_aethersx2() {
     homeDIR=$home; sed -i s+'/home/pi/'+"$homeDIR/"+g "aethersx2.sh"
     chmod 755 "aethersx2.sh"; mv "aethersx2.sh" "$md_inst"
     chmod 755 "AetherSX2.desktop"; cp "AetherSX2.desktop" "$md_inst"; cp "AetherSX2.desktop" "/usr/share/applications/"
-    mv "AetherSX2.desktop" "$home/Desktop"; chown "$(basename $home)" "$home/Desktop/AetherSX2.desktop"
+    mv "AetherSX2.desktop" "$home/Desktop"; chown $__user:$__user "$home/Desktop/AetherSX2.desktop"
     mv "AetherSX2-128.xpm" "$md_inst"; mv "PS2BIOSRequired.jpg" "$md_inst"
 
     if [[ ! -d "$home/.config/aethersx2" ]]; then mkdir "$home/.config/aethersx2"; fi
@@ -46,18 +49,18 @@ function install_aethersx2() {
     if [[ ! -f "$home/.config/aethersx2/gamesettings/SLUS-20946_2C6BE434.ini" ]]; then mv "SLUS-20946_2C6BE434.ini" "$home/.config/aethersx2/gamesettings"; fi
     if [[ ! -d "$home/.config/aethersx2/covers" ]]; then mkdir "$home/.config/aethersx2/covers"; fi
     if [[ ! -f "$home/.config/aethersx2/covers/uLaunchELF 4.42d.png" ]]; then mv 'uLaunchELF 4.42d.png' "$home/.config/aethersx2/covers"; fi
-    chown "$(basename $home)" -R "$home/.config/aethersx2"
+    chown -R $__user:$__user -R "$home/.config/aethersx2"
 
     if [[ ! -d "$home/RetroPie/roms/ps2" ]]; then mkdir "$home/RetroPie/roms/ps2"; fi
     chmod 755 '+Start AetherSX2.sh'; mv '+Start AetherSX2.sh' "$home/RetroPie/roms/ps2"
     if [[ ! -f "$home/RetroPie/roms/ps2/gamelist.xml" ]]; then mv 'gamelist.xml' "$home/RetroPie/roms/ps2"; fi
     if [[ ! -d "$home/RetroPie/roms/ps2/media" ]]; then mv 'media' "$home/RetroPie/roms/ps2"; fi
-    chown "$(basename $home)" -R "$home/RetroPie/roms/ps2"
+    chown -R $__user:$__user -R "$home/RetroPie/roms/ps2"
 }
 
 function configure_aethersx2() {
-    addSystem "ps2"
-    addEmulator "$md_id" "aethersx2" "ps2" "XINIT: /opt/retropie/emulators/aethersx2/aethersx2.sh %ROM%"
+    addEmulator "$md_id" "aethersx2" "ps2" "XINIT:/opt/retropie/emulators/aethersx2/aethersx2.sh %ROM%"
 	if [[ $(cat /opt/retropie/configs/ps2/emulators.cfg | grep -q 'default =' ; echo $?) == '1' ]]; then echo 'default = "aethersx2"' >> /opt/retropie/configs/ps2/emulators.cfg; fi
 	sed -i 's/default\ =.*/default\ =\ \"aethersx2\"/g' /opt/retropie/configs/ps2/emulators.cfg
+    addSystem "ps2"
 }
---
+++ ext/RetroPie-Extra/scriptmodules/emulators/dolphin-rpi.sh
@@ -0,0 +1,191 @@
+#!/usr/bin/env bash
+ 
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+# If no user is specified (for RetroPie below v4.8.9)
+if [[ -z "$__user" ]]; then __user="$SUDO_USER"; [[ -z "$__user" ]] && __user="$(id -un)"; fi
+
+# Script Provided by gvx64 - Updated by RapidEdwin08 20250311
+# https://retropie.org.uk/forum/topic/36971/gamecube-wii-error-mario-part-5/2
+
+rp_module_id="dolphin-rpi"
+rp_module_desc="Gamecube/Wii Emulator Dolphin v5.0-4544 Optimized for Pi"
+rp_module_help="ROM Extensions: .elf .dol .gcm .iso .rvz .wbfs .ciso .gcz .wad .dff\n\nCopy your Gamecube roms to $romdir/gc and Wii roms to $romdir/wii \n \nSample [Hotkeys.ini] :\n$home/DolphinConfig5.0/Config/Hotkeys.ini\n \nKeys/Toggle Pause = \`Button 6\` & \`Button 3\`\nKeys/Stop = \`Button 6\` & \`Button 1\`\nKeys/Reset = \`Button 6\` & \`Button 0\`\nKeys/Toggle Fullscreen = \`Button 6\` & \`Button 2\`\nKeys/Take Screenshot = \`Button 6\` & \`Button 8\`\nKeys/Exit = \`Button 6\` & \`Button 7\`\n \nExamples of Games [dolphin-rpi] is Optimized for:\nMario Kart Double Dash\nResident Evil 4\nMario Golf Toadstool Tour\nMetroid Prime\nZelda Twilight Princess (Gamecube)\nBomberman Generations\nSonic Adventure DX\nThe Last Story (Wii)\nand more..."
+rp_module_licence="GPL2 https://github.com/gvx64/dolphin-rpi/blob/master/license.txt"
+rp_module_repo="git https://github.com/gvx64/dolphin-rpi.git master"
+rp_module_section="exp"
+rp_module_flags="!all 64bit"
+
+function depends_dolphin-rpi() {
+    local depends=(cmake gcc-11 g++-11 pkg-config libasound2-dev libopenal-dev libevdev-dev libgtk2.0-dev qtbase5-private-dev libxxf86vm-dev x11proto-xinerama-dev libsdl2-dev)
+ 
+    isPlatform "kms" && depends+=(xorg matchbox-window-manager)
+ 
+    getDepends "${depends[@]}"
+}
+ 
+function sources_dolphin-rpi() {
+    gitPullOrClone
+    # Hide Font Error: Trying to access Windows-1252 fonts but they are not loaded. /Source/Core/Core/HW/EXI/EXI_DeviceIPL.cpp
+    applyPatch "$md_data/01_font_alerts.diff"
+}
+ 
+function build_dolphin-rpi() {
+    mkdir build
+    cd build
+    # use the bundled 'speexdsp' libs, distro versions before 1.2.1 trigger a 'cmake' error
+    cmake .. -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11 -DBUNDLE_SPEEX=ON -DENABLE_AUTOUPDATE=OFF -DENABLE_ANALYTICS=OFF  -DUSE_DISCORD_PRESENCE=O>    make clean
+    make
+    md_ret_require="$md_build/build/Binaries/dolphin-emu"
+}
+ 
+function install_dolphin-rpi() {
+    #copy binaries to /opt/retropie/emulators/dolphin-rpi
+    cd build/Binaries/
+    mkdir /opt/retropie/emulators/dolphin-rpi/
+    mkdir /opt/retropie/emulators/dolphin-rpi/bin/
+    mv dolphin-emu /opt/retropie/emulators/dolphin-rpi/bin/
+    mv dolphin-emu-nogui /opt/retropie/emulators/dolphin-rpi/bin/
+    #use $home/DolphinConfig5.0/ as the configuration/settings/save file directory
+    cd ..
+    mkdir $home/DolphinConfig5.0/
+    mkdir $home/DolphinConfig5.0/Config/
+    mv ../Data/Sys/GameSettings/ $home/DolphinConfig5.0/
+    mv ../Data/Sys/GC/ $home/DolphinConfig5.0/
+    mv ../Data/Sys/Wii/ $home/DolphinConfig5.0/
+    mv ../Data/Sys/Maps/ $home/DolphinConfig5.0/
+    mv ../Data/Sys/Resources/ $home/DolphinConfig5.0/
+    mv ../Data/Sys/Shaders/ $home/DolphinConfig5.0/
+    mv ../Data/Sys/Themes/ $home/DolphinConfig5.0/
+    mv ../Data/Sys/codehandler.bin $home/DolphinConfig5.0/
+    mv ../Data/Sys/totaldb.dsy $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-de.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-en.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-es.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-fr.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-it.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-ja.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-ko.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-nl.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-pt.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-ru.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-zh_CN.txt $home/DolphinConfig5.0/
+    mv ../Data/Sys/wiitdb-zh_TW.txt $home/DolphinConfig5.0/
+}
+ 
+function remove_dolphin-rpi() {
+    rm -r $home/DolphinConfig5.0/GameSettings/
+    rm -r $home/DolphinConfig5.0/Maps/
+    rm -r $home/DolphinConfig5.0/Resources/
+    rm -r $home/DolphinConfig5.0/Shaders/
+    rm -r $home/DolphinConfig5.0/Themes/
+    rm -r $home/DolphinConfig5.0/Config/
+    rm $home/DolphinConfig5.0/codehandler.bin
+    rm $home/DolphinConfig5.0/totaldb.dsy
+    rm $home/DolphinConfig5.0/wiitdb-de.txt
+    rm $home/DolphinConfig5.0/wiitdb-en.txt
+    rm $home/DolphinConfig5.0/wiitdb-es.txt
+    rm $home/DolphinConfig5.0/wiitdb-fr.txt
+    rm $home/DolphinConfig5.0/wiitdb-it.txt
+    rm $home/DolphinConfig5.0/wiitdb-ja.txt
+    rm $home/DolphinConfig5.0/wiitdb-ko.txt
+    rm $home/DolphinConfig5.0/wiitdb-nl.txt
+    rm $home/DolphinConfig5.0/wiitdb-pt.txt
+    rm $home/DolphinConfig5.0/wiitdb-ru.txt
+    rm $home/DolphinConfig5.0/wiitdb-zh_CN.txt
+    rm $home/DolphinConfig5.0/wiitdb-zh_TW.txt
+#   Do not delete GC or Wii save file directories upon emulator uninstall
+#    rm -r $home/DolphinConfig5.0/GC/
+#    rm -r $home/DolphinConfig5.0/Wii/
+#    rm -r $home/DolphinConfig5.0/
+
+    # Remove Symbolic Link to Address Error: Could not find resource: /usr/local/share/dolphin-emu/sys/Resources/nobanner.png
+    rm -Rf /usr/local/share/dolphin-emu
+}
+ 
+function configure_dolphin-rpi() {
+    mkRomDir "gc"
+    mkRomDir "wii"
+ 
+    local launch_prefix
+    isPlatform "kms" && launch_prefix="XINIT-WM:"
+ 
+    addEmulator 0 "$md_id" "gc" "$launch_prefix$md_inst/bin/dolphin-emu-nogui -e %ROM% -u $home/DolphinConfig5.0/"
+    addEmulator 1 "$md_id-gui" "gc" "$launch_prefix$md_inst/bin/dolphin-emu -b -e %ROM% -u $home/DolphinConfig5.0/"
+    addEmulator 2 "$md_id-editor" "gc" "XINIT-WMC:$md_inst/bin/dolphin-emu -u $home/DolphinConfig5.0/"
+    #addEmulator 3 "$md_id-editor-hotkeys" "gc" "XINIT-WMC:$md_inst/bin/dolphin-emu-qt2 -u $home/DolphinConfig5.0/"
+    addEmulator 0 "$md_id" "wii" "$launch_prefix$md_inst/bin/dolphin-emu-nogui -e %ROM% -u $home/DolphinConfig5.0/"
+    addEmulator 1 "$md_id-gui" "wii" "$launch_prefix$md_inst/bin/dolphin-emu -b -e %ROM% -u $home/DolphinConfig5.0/"
+    addEmulator 2 "$md_id-editor" "wii" "XINIT-WMC:$md_inst/bin/dolphin-emu -u $home/DolphinConfig5.0/"
+    #addEmulator 3 "$md_id-editor-hotkeys" "wii" "XINIT-WMC:$md_inst/bin/dolphin-emu-qt2 -u $home/DolphinConfig5.0/"
+ 
+    addSystem "gc"
+    addSystem "wii"
+ 
+    [[ "$md_mode" == "remove" ]] && return
+ 
+    # preset options used for Raspberry Pi 4 (Bookworm) - modify for your build as desired
+   cat >"$home/DolphinConfig5.0/Config/Dolphin.ini" <<_EOF_
+[Core]
+OverclockEnable = False
+EnableCheats = False
+GFXBackend = Vulkan
+CPUCore = 4
+Fastmem = True
+CPUThread = True
+SyncOnSkipIdle = True
+FPRF = False
+AccurateNaNs = False
+AudioLatency = 20
+AutoDiscChange = True
+[Display]
+FullscreenDisplayRes = Auto
+Fullscreen = True
+RenderToMain = True
+KeepWindowOnTop = True
+[Interface]
+ConfirmStop = False
+[General]
+ISOPath0 = "$home/RetroPie/roms/gc"
+ISOPath1 = "$home/RetroPie/roms/wii"
+ISOPaths = 2
+WiiSDCardPath = $home/DolphinConfig5.0/Wii/sd.raw
+_EOF_
+
+    # Complimentary RE4 INI options - modify for your version as desired
+    # G4BD08  Resident Evil 4 (PAL)
+    # G4BE08  Resident Evil 4 (NTSC-U)
+    # G4BP08  Resident Evil 4 (PAL)
+    # RB4E08  Resident Evil 4: Wii Edition (NTSC-U)
+    # RB4P08  Resident Evil 4: Wii Edition (PAL)
+    # RB4X08  Resident Evil 4: Wii Edition (PAL) 
+   if [ ! -f $home/DolphinConfig5.0/GameSettings/G4BE08.ini ]; then cat >"$home/DolphinConfig5.0/GameSettings/G4BE08.ini" <<_EOF_; fi
+# G4BE08 - Resident Evil 4 (NTSC-U)
+[Core]
+GFXBackend = Vulkan
+CPUThread = True
+EmulationSpeed = 1.
+SyncGPU = True
+[Display]
+Fullscreen = True
+[Video_Hacks]
+VISkip = True
+[Video_Hardware]
+VSync = False
+[Video_Settings]
+AspectRatio = 3
+_EOF_
+
+   chown -R $__user:$__user "$home/DolphinConfig5.0"
+   
+   # Symbolic Link to Address Error: Could not find resource: /usr/local/share/dolphin-emu/sys/Resources/nobanner.png
+   if [ ! -d /usr/local/share/dolphin-emu ]; then sudo mkdir /usr/local/share/dolphin-emu; fi
+   if [ ! -d /usr/local/share/dolphin-emu/sys ]; then sudo mkdir /usr/local/share/dolphin-emu/sys; fi
+   if [ ! -d /usr/local/share/dolphin-emu/sys/Resources ]; then sudo ln -s $home/DolphinConfig5.0/Resources /usr/local/share/dolphin-emu/sys/Resources; fi
+}
