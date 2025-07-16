2d53b884432c096235a1d8fd543bcdf8c251669f
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-07-16 07:53:35 -0400

    Add files via upload

--- ext/RetroPie-Extra/scriptmodules/emulators/pcsx2.sh
+++ ext/RetroPie-Extra/scriptmodules/emulators/pcsx2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is NOT part of The RetroPie Project
 #
 # The RetroPie Project is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
@@ -8,11 +8,11 @@
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
-# Remove or Replace PCSX2 script in: $home/RetroPie-Setup/scriptmodules/emulators/pcsx2.sh
-#
+# If no user is specified (for RetroPie below v4.8.9)
+if [[ -z "$__user" ]]; then __user="$SUDO_USER"; [[ -z "$__user" ]] && __user="$(id -un)"; fi
 
 rp_module_id="pcsx2"
-rp_module_desc="PS2 emulator PCSX2"
+rp_module_desc="PS2 emulator PCSX2 Optimized for x86_64"
 rp_module_help="PCSX2 is an open source PS2 Emulator                                                                                                                                                                                                     \n \n pcsx2-v2.0.2-linux-appimage-x64-Qt.AppImage \n \n https://pcsx2.net \n \n PCSX2 uses third-party code. \n You can view the licenses for this code by selecting \"Third-Party Notices\" \n ROM Extensions: .bin .iso .img .mdf .z .z2 .bz2 .cso .chd .ima .gz\n\nCopy your PS2 roms to $romdir/ps2\n\nCopy the required BIOS file to $biosdir"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/PCSX2/pcsx2/master/COPYING.GPLv3"
 rp_module_section="exp"
@@ -37,7 +37,7 @@ function install_pcsx2() {
     homeDIR=$home; sed -i s+'/home/pi/'+"$homeDIR/"+g "pcsx2.sh"
     chmod 755 "pcsx2.sh"; mv "pcsx2.sh" "$md_inst"
     chmod 755 "PCSX2.desktop"; cp "PCSX2.desktop" "$md_inst"; cp "PCSX2.desktop" "/usr/share/applications/"
-    mv "PCSX2.desktop" "$home/Desktop"; chown "$(basename $home)" "$home/Desktop/PCSX2.desktop"
+    mv "PCSX2.desktop" "$home/Desktop"; chown $__user:$__user "$home/Desktop/PCSX2.desktop"
     mv "PCSX2-128.xpm" "$md_inst"; mv "PS2BIOSRequired.jpg" "$md_inst"
 
     if [[ ! -d "$home/.config/PCSX2" ]]; then mkdir "$home/.config/PCSX2"; fi
@@ -51,18 +51,18 @@ function install_pcsx2() {
     if [[ ! -d "$home/.config/PCSX2/gamesettings" ]]; then mkdir "$home/.config/PCSX2/gamesettings"; fi
     if [[ ! -d "$home/.config/PCSX2/covers" ]]; then mkdir "$home/.config/PCSX2/covers"; fi
     if [[ ! -f "$home/.config/PCSX2/covers/uLaunchELF 4.42d.png" ]]; then mv 'uLaunchELF 4.42d.png' "$home/.config/PCSX2/covers"; fi
-    chown "$(basename $home)" -R "$home/.config/PCSX2"
+    chown -R $__user:$__user "$home/.config/PCSX2"
 
     if [[ ! -d "$home/RetroPie/roms/ps2" ]]; then mkdir "$home/RetroPie/roms/ps2"; fi
     chmod 755 '+Start PCSX2.sh'; mv '+Start PCSX2.sh' "$home/RetroPie/roms/ps2"
     if [[ ! -f "$home/RetroPie/roms/ps2/gamelist.xml" ]]; then mv 'gamelist.xml' "$home/RetroPie/roms/ps2"; fi
     if [[ ! -d "$home/RetroPie/roms/ps2/media" ]]; then mv 'media' "$home/RetroPie/roms/ps2"; fi
-    chown "$(basename $home)" -R "$home/RetroPie/roms/ps2"
+    chown -R $__user:$__user "$home/RetroPie/roms/ps2"
 }
 
 function configure_pcsx2() {
-    addSystem "ps2"
-    addEmulator "$md_id" "pcsx2" "ps2" "XINIT: /opt/retropie/emulators/pcsx2/pcsx2.sh %ROM%"
+    addEmulator "$md_id" "pcsx2" "ps2" "XINIT:/opt/retropie/emulators/pcsx2/pcsx2.sh %ROM%"
     if [[ $(cat /opt/retropie/configs/ps2/emulators.cfg | grep -q 'default =' ; echo $?) == '1' ]]; then echo 'default = "pcsx2"' >> /opt/retropie/configs/ps2/emulators.cfg; fi
     sed -i 's/default\ =.*/default\ =\ \"pcsx2\"/g' /opt/retropie/configs/ps2/emulators.cfg
+    addSystem "ps2"
 }
