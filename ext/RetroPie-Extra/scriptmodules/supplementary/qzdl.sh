95ec947c8406a8ab4403d58f60746d6434d27432
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-09-04 08:27:39 -0400

    Add files via upload

---
+++ ext/RetroPie-Extra/scriptmodules/supplementary/qzdl.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+# RetroPie-Extra
+# https://raw.githubusercontent.com/RapidEdwin08/RetroPie-Setup/master/ext/RetroPie-Extra/LICENSE
+#
+# If no user is specified (for RetroPie below v4.8.9)
+if [[ -z "$__user" ]]; then __user="$SUDO_USER"; [[ -z "$__user" ]] && __user="$(id -un)"; fi
+
+rp_module_id="qzdl"
+rp_module_desc="Qt version of ZDL - ZDoom [WAD] Launcher"
+rp_module_help="- General Settings [Source Ports]:\n/opt/retropie/ports/gzdoom/gzdoom\n/opt/retropie/ports/lzdoom/lzdoom\n \n- General Settings [IWADs]:\n$home/RetroPie/roms/ports/doom/doomu.wad\n$home/RetroPie/roms/ports/doom/doom2.wad\n \n- General Settings [Always Add These Parameters]:\nDOOMWADDIR=$home/RetroPie/roms/ports/doom\n-config $home/RetroPie/roms/ports/doom/gzdoom.ini\n+gl_maplightmode 8\n \nExtra Command Line Arguments: +logfile /dev/shm/ZDL.log"
+rp_module_licence="GNU3 https://raw.githubusercontent.com/qbasicer/qzdl/refs/heads/master/LICENSE"
+rp_module_repo="git https://github.com/qbasicer/qzdl.git master"
+rp_module_section="exp"
+rp_module_flags=""
+
+function depends_qzdl() {
+    #local depends=(qtcreator qtdeclarative5-dev)
+    local depends=(cmake qtbase5-dev qt5-qmake qtbase5-dev-tools qtchooser)
+    if [[ ! $(apt-cache search qt5-default) == '' ]]; then
+        depends+=(qt5-default)
+    fi
+    isPlatform "kms" && depends+=(xorg matchbox-window-manager)
+    echo DEPENDS: "${depends[@]}"
+    getDepends "${depends[@]}"
+}
+
+function sources_qzdl() {
+    gitPullOrClone
+}
+
+function build_qzdl() {
+    cd $md_build
+    mkdir build
+    cd build
+    cmake ..
+    make -j"$(nproc)"
+    md_ret_require="$md_build/build/zdl"
+}
+
+function install_qzdl() {
+    md_ret_files=(
+        'build/zdl'
+        'bmp_logo.xpm'
+        'ico_icon.xpm'
+    )
+}
+
+function remove_qzdl() {
+    if [[ -f /usr/share/applications/ZDL.desktop ]]; then sudo rm -f /usr/share/applications/ZDL.desktop; fi
+    if [[ -f "$home/Desktop/ZDL.desktop" ]]; then rm "$home/Desktop/ZDL.desktop"; fi
+    if [[ -f "$home/RetroPie/roms/ports/+Start ZDL.sh" ]]; then rm "$home/RetroPie/roms/ports/+Start ZDL.sh"; fi
+}
+
+function configure_qzdl() {
+    mkRomDir "ports"
+    local launch_prefix
+    isPlatform "kms" && launch_prefix="XINIT-WMC:"
+    addPort "$md_id" "zdl" "+Start ZDL" "$launch_prefix$md_inst/zdl"
+    sed -i s'+_PORT_+_SYS_+g' "$romdir/ports/+Start ZDL.sh"
+
+    cat >"$md_inst/ZDL.desktop" << _EOF_
+[Desktop Entry]
+Name=ZDL
+GenericName=ZDL
+Comment=ZDoom WAD Launcher
+Exec=$md_inst/zdl
+Icon=$md_inst/ico_icon.xpm
+Terminal=false
+Type=Application
+Categories=Game;Emulator
+Keywords=ZDL;ZDoom;WAD;Launcher
+StartupWMClass=ZDL
+Name[en_US]=ZDL
+_EOF_
+    chmod 755 "$md_inst/ZDL.desktop"
+    if [[ -d "$home/Desktop" ]]; then cp "$md_inst/ZDL.desktop" "$home/Desktop/ZDL.desktop"; chown $__user:$__user "$home/Desktop/ZDL.desktop"; fi
+    mv "$md_inst/ZDL.desktop" "/usr/share/applications/ZDL.desktop"
+
+    [[ "$md_mode" == "remove" ]] && remove_qzdl
+}
