0de5622777107a78df182e4b8f2db58706a70938
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-10-11 10:21:31 -0400

    Update prboom-plus.sh

--- ext/RetroPie-Extra/scriptmodules/ports/prboom-plus.sh
+++ ext/RetroPie-Extra/scriptmodules/ports/prboom-plus.sh
@@ -16,6 +16,7 @@ if [[ -z "$__user" ]]; then __user="$SUDO_USER"; [[ -z "$__user" ]] && __user="$
 
 rp_module_id="prboom-plus"
 rp_module_desc="Doom/Doom II engine - Enhanced PRBoom Port"
+rp_module_help="*[libpcre3-dev] HAS BEEN DEPRECATED SINCE 202503*\n \n[libpcre3-dev] is Required for PRBoom-Plus\n \nTHIS SCRIPT CAN/WILL INSTALL [libpcre3-dev] FROM .DEBs\n \n[libpcre3-dev]*.DEBs CAN BE UNINSTALLED MANUALLY WITH:\n \nsudo dpkg -r libpcre3-dbg libpcre16-3 libpcrecpp0v5 libpcre32-3 libpcre3-dev"
 rp_module_licence="https://github.com/coelckers/prboom-plus"
 rp_module_repo="git https://github.com/coelckers/prboom-plus.git master"
 rp_module_section="exp"
@@ -23,11 +24,35 @@ rp_module_section="exp"
 function depends_prboom-plus() {
     #local depends=(libsdl2-dev libsdl2-net-dev libsdl2-image-dev libsdl2-mixer-dev libfluidsynth-dev libportmidi-dev libmad0-dev libdumb1-dev libvorbis-dev)
     local depends=(libsdl2-dev libsdl2-net-dev libsdl2-image-dev libsdl2-mixer-dev libfluidsynth-dev libportmidi-dev libmad0-dev libdumb1-dev libvorbis-dev zlib1g-dev libogg-dev)
+    # pcre3 was Removed from Trixie in between February-March of 2025
     if [[ $(apt-cache search libpcre3-dev | grep 'libpcre3-dev ') == '' ]]; then
-        depends+=(libpcre2-dev)
+        #depends+=(libpcre2-dev)
+        local pcre3_arch=$(dpkg --print-architecture)
+        wget http://http.us.debian.org/debian/pool/main/p/pcre3/libpcrecpp0v5_8.39-15_$pcre3_arch.deb -P /tmp
+        wget http://http.us.debian.org/debian/pool/main/p/pcre3/libpcre3-dbg_8.39-15_$pcre3_arch.deb -P /tmp
+        wget http://http.us.debian.org/debian/pool/main/p/pcre3/libpcre16-3_8.39-15_$pcre3_arch.deb -P /tmp
+        wget http://http.us.debian.org/debian/pool/main/p/pcre3/libpcre32-3_8.39-15_$pcre3_arch.deb -P /tmp
+        wget http://http.us.debian.org/debian/pool/main/p/pcre3/libpcre3-dev_8.39-15_$pcre3_arch.deb -P /tmp
+
+        # Retain this Installation 0rder...
+        dpkg -i /tmp/libpcrecpp0v5_8.39-15_$pcre3_arch.deb
+        dpkg -i /tmp/libpcre3-dbg_8.39-15_$pcre3_arch.deb
+        dpkg -i /tmp/libpcre16-3_8.39-15_$pcre3_arch.deb
+        dpkg -i /tmp/libpcre32-3_8.39-15_$pcre3_arch.deb
+        dpkg -i /tmp/libpcre3-dev_8.39-15_$pcre3_arch.deb
+
+        rm -f /tmp/libpcrecpp0v5_8.39-15_$pcre3_arch.deb
+        rm -f /tmp/libpcre3-dbg_8.39-15_$pcre3_arch.deb
+        rm -f /tmp/libpcre16-3_8.39-15_$pcre3_arch.deb
+        rm -f /tmp/libpcre32-3_8.39-15_$pcre3_arch.deb
+        rm -f /tmp/libpcre3-dev_8.39-15_$pcre3_arch.deb
+
+        echo *PCRE3 HAS BEEN REMOVED FROM TRIXIE AND WILL NO LONGER BE UPDATED*
+        echo LIBPCRE3*.DEBs CAN BE UNINSTALLED MANUALLY WITH: [sudo dpkg -r libpcre3-dbg libpcre16-3 libpcrecpp0v5 libpcre32-3 libpcre3-dev]
     else
         depends+=(libpcre3-dev)
     fi
+    [[ "$__gcc_version" -gt 12 ]] && depends+=(gcc-12 g++-12)
     getDepends "${depends[@]}"
 }
 
@@ -39,6 +64,7 @@ function build_prboom-plus() {
     cd prboom2
 #    ./bootstrap
 #    ./configure
+    if [[ "$__gcc_version" -gt 12 ]]; then export CC=/usr/bin/gcc-12; export CXX=/usr/bin/g++-12; fi
     cmake .
     make
     md_ret_require="$md_build/prboom2/prboom-plus"
