9a944e3c145e57af592fa9906ba5edb443c01d7e
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-10-28 21:09:50 -0400

    Update uzdoom.sh

--- ext/RetroPie-Extra/scriptmodules/ports/uzdoom.sh
+++ ext/RetroPie-Extra/scriptmodules/ports/uzdoom.sh
@@ -14,8 +14,7 @@
 rp_module_id="uzdoom"
 rp_module_desc="UZDoom is a modder-friendly OpenGL and Vulkan source port based on the DOOM engine"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/ZDoom/uzdoom/master/LICENSE"
-#rp_module_repo="git https://github.com/UZDoom/UZDoom.git trunk"
-rp_module_repo="git https://github.com/UZDoom/UZDoom.git trunk 5104cc43"
+rp_module_repo="git https://github.com/UZDoom/UZDoom.git trunk d1d12cfc"
 rp_module_section="exp"
 rp_module_flags="sdl2 !armv6"
 
@@ -37,7 +36,6 @@ function sources_uzdoom() {
     fi
 
     # Apply JoyPad + Preference Tweaks
-    applyPatch "$md_data/01_EndoomOff.diff"
     applyPatch "$md_data/02_JoyMappings.diff"
     applyPatch "$md_data/03_Preferences.diff"
 
