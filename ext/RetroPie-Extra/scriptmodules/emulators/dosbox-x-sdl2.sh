6376822c4dae0e0a3bdc3254571458854d821f31
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-11-12 00:18:02 -0500

    Update dosbox-x-sdl2.sh

--- ext/RetroPie-Extra/scriptmodules/emulators/dosbox-x-sdl2.sh
+++ ext/RetroPie-Extra/scriptmodules/emulators/dosbox-x-sdl2.sh
@@ -116,8 +116,8 @@ function configure_dosbox-x-sdl2() {
     local dosbox_ver="$(cat $md_inst/share/metainfo/com.dosbox_x.DOSBox-X.metainfo.xml | grep 'release version=' | awk '{ print $2}' | cut -d"=" -f 2- | sed 's/"//g')"
     local dosbox_conf="$md_conf_root/pc/dosbox-x-$dosbox_ver.conf"
     if [[ -f "$dosbox_conf" ]]; then
-        sed -i 's+^cycles.*+cycles             = max+g' "$dosbox_conf"
-        sed -i 's+^cpu_cycles.*+cpu_cycles           = max+g' "$dosbox_conf"
+        sed -i 's+^cycles .*+cycles             = max+g' "$dosbox_conf"
+        sed -i 's+^cpu_cycles .*+cpu_cycles           = max+g' "$dosbox_conf"
         chown $__user:$__user "$dosbox_conf"
     fi
 
