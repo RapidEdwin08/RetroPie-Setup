92b45134c5d96a943622bdf61007087291a96da7
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-11-12 00:16:06 -0500

    Update dosbox-staging-dev.sh

--- ext/RetroPie-Extra/scriptmodules/emulators/dosbox-staging-dev.sh
+++ ext/RetroPie-Extra/scriptmodules/emulators/dosbox-staging-dev.sh
@@ -110,8 +110,8 @@ function configure_dosbox-staging-dev() {
     # dosbox-staging.conf # cycles    = auto -->> cycles    = max
     local dosbox_conf="$md_conf_root/pc/dosbox-staging.conf"
     if [[ -f "$dosbox_conf" ]]; then
-        sed -i 's+^cycles.*+cpu_cycles           = max+g' "$dosbox_conf"
-        sed -i 's+^cpu_cycles.*+cpu_cycles           = max+g' "$dosbox_conf"
+        sed -i 's+^cycles .*+cpu_cycles           = max+g' "$dosbox_conf"
+        sed -i 's+^cpu_cycles .*+cpu_cycles           = max+g' "$dosbox_conf"
         # fullresolution = original -->> fullscreen_mode = original
         sed -i 's+^fullresolution.*+fullscreen_mode = original+g' "$dosbox_conf"
         chown $__user:$__user "$dosbox_conf"
