9ce85d7c012504bf2e5139821f24e566f10bdc15
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-11-12 17:10:49 -0500

    Update dosbox-staging.sh

--- scriptmodules/emulators/dosbox-staging.sh
+++ scriptmodules/emulators/dosbox-staging.sh
@@ -104,6 +104,7 @@ function remove_dosbox-staging() {
 
 function configure_dosbox-staging() {
     configure_dosbox
+    addEmulator "1" "$md_id" "pc" "$md_inst/dosbox-staging.sh %ROM%" # Overwrite prior [configure_dosbox] entry that pointed to /roms/pc/+Start DOSBox-Staging
     if [[ -d "$romdir/pc" ]]; then chown -R $__user:$__user "$romdir/pc"; fi
 
     [[ "$md_mode" == "remove" ]] && remove_dosbox-staging
@@ -114,28 +115,34 @@ function configure_dosbox-staging() {
     chown -R $__user:$__user "$romdir/pc/.games"
 
     cp "$romdir/pc/+Start DOSBox-Staging.sh" "$md_inst/dosbox-staging.sh"; chmod 755 "$md_inst/dosbox-staging.sh"
-    sed -i 's+\[\[ -n "$DISPLAY" \]\] \&\& params\+=(-fullscreen)+if \[\[ ! "$0" == "/opt/retropie/emulators/dosbox-staging/dosbox-staging.sh" \]\] \&\& \[\[ -n "$DISPLAY" \]\]; then params\+=(-fullscreen); fi+g' "$md_inst/dosbox-staging.sh"
+    #sed -i 's+\[\[ -n "$DISPLAY" \]\] \&\& params\+=(-fullscreen)+if \[\[ ! "$0" == "/opt/retropie/emulators/dosbox-staging-dev/dosbox-staging.sh" \]\] \&\& \[\[ -n "$DISPLAY" \]\]; then params\+=(-fullscreen); fi+g' "$md_inst/dosbox-staging.sh"
+    sed -i 's+\[\[ -n "$DISPLAY" \]\] \&\& params\+=(-fullscreen)+if \[\[ ! "$1" == "" \]\] \&\& \[\[ -n "$DISPLAY" \]\]; then params\+=(-fullscreen); fi+g' "$md_inst/dosbox-staging.sh"
 
     local config_dir="$md_conf_root/pc"
+    local shell_history="$config_dir/shell_history.txt"
+    if [[ ! -f "$shell_history" ]]; then cat > "$shell_history" << _EOF_; fi
+exit
+intro
+_EOF_
     chown -R "$__user":"$__group" "$config_dir"
 
-    local staging_output="texturenb"
+    local staging_output="opengl"
     if isPlatform "kms"; then
-        staging_output="openglnb"
+        staging_output="texturenb" # openglnb Deprecated value
     fi
 
     local config_path=$(su "$__user" -c "\"$md_inst/bin/dosbox\" -printconf")
     if [[ -f "$config_path" ]]; then
         iniConfig " = " "" "$config_path"
         iniSet "cpu_cycles" "max"
+        iniSet "output" "$staging_output"
+        iniSet "fullresolution" "original"
+        iniSet "vsync" "true"
+        iniSet "blocksize" "2048"
+        iniSet "prebuffer" "50"
         if isPlatform "rpi"; then
-            iniSet "fullscreen" "true"
-            iniSet "fullresolution" "original"
-            iniSet "vsync" "true"
-            iniSet "output" "$staging_output"
             iniSet "core" "dynamic"
-            iniSet "blocksize" "2048"
-            iniSet "prebuffer" "50"
+            iniSet "fullscreen" "true"
         fi
     fi
 
