f6962413dabe5ecf76f8448af40f4a623b1d1da3
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2026-01-16 06:16:21 -0500

    Update wifi.sh

--- scriptmodules/supplementary/wifi.sh
+++ scriptmodules/supplementary/wifi.sh
@@ -8,68 +8,11 @@
 # See the LICENSE.md file at the top-level directory of this distribution and
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
-# Choose a [signal_quality_method] for [list_wifi] quality:
-#   dBm_no_conversion
-#   dBm_to_percent_A   # Simple Linear Conversion by David Manpearl
-#   dBm_to_percent_B   # Double Logarithm of Signal Power Conversion by Artfaith
 
 rp_module_id="wifi"
 rp_module_desc="Configure WiFi"
 rp_module_section="config"
 rp_module_flags="!x11"
-
-function dBm_no_conversion {
-# Retain a (SINGLE_FIELD) here due to [awk '{print $2}'] used below for [$essid] Detection
-#echo "($1)"
-echo "($1_dBm)"
-#echo "($1dBm)"
-#echo "(dBm$1)"
-}
-
-# https://stackoverflow.com/questions/15797920/how-to-convert-wifi-signal-strength-from-quality-percent-to-rssi-dbm
-function dBm_to_percent_A { # Convert dBm to percentage ( quality = 2 * (dBm + 100)  where dBm: [-100 to -50] ); Apr 3, 2013 at 20:59 David Manpearl
-if [[ "$1" -lt '-100' ]]; then dBm_as_percent=0
-elif [[ "$1" -gt '-50' ]]; then dBm_as_percent=100
-else dBm_as_percent=$(( 2 * $(( $1 + 100 )) ))
-fi
-
-# Retain a (SINGLE_FIELD) here due to [awk '{print $2}'] used below for [$essid] Detection
-echo "[%$dBm_as_percent]"
-}
-
-# https://stackoverflow.com/questions/15797920/how-to-convert-wifi-signal-strength-from-quality-percent-to-rssi-dbm
-function dBm_to_percent_B { # Convert dBm to percentage (based on https://www.adriangranados.com/blog/dbm-to-percent-conversion); Oct 12, 2018 at 12:08 Artfaith
-  dbmtoperc_d=$(echo "$1" | tr -d -)
-  dbmtoperc_r=0
-  if [[ "$dbmtoperc_d" =~ [0-9]+$ ]]; then
-    if ((1<=$dbmtoperc_d && $dbmtoperc_d<=20)); then dbmtoperc_r=100
-    elif ((21<=$dbmtoperc_d && $dbmtoperc_d<=23)); then dbmtoperc_r=99
-    elif ((24<=$dbmtoperc_d && $dbmtoperc_d<=26)); then dbmtoperc_r=98
-    elif ((27<=$dbmtoperc_d && $dbmtoperc_d<=28)); then dbmtoperc_r=97
-    elif ((29<=$dbmtoperc_d && $dbmtoperc_d<=30)); then dbmtoperc_r=96
-    elif ((31<=$dbmtoperc_d && $dbmtoperc_d<=32)); then dbmtoperc_r=95
-    elif ((33==$dbmtoperc_d)); then dbmtoperc_r=94
-    elif ((34<=$dbmtoperc_d && $dbmtoperc_d<=35)); then dbmtoperc_r=93
-    elif ((36<=$dbmtoperc_d && $dbmtoperc_d<=38)); then dbmtoperc_r=$((92-($dbmtoperc_d-36)))
-    elif ((39<=$dbmtoperc_d && $dbmtoperc_d<=51)); then dbmtoperc_r=$((90-($dbmtoperc_d-39)))
-    elif ((52<=$dbmtoperc_d && $dbmtoperc_d<=55)); then dbmtoperc_r=$((76-($dbmtoperc_d-52)))
-    elif ((56<=$dbmtoperc_d && $dbmtoperc_d<=58)); then dbmtoperc_r=$((71-($dbmtoperc_d-56)))
-    elif ((59<=$dbmtoperc_d && $dbmtoperc_d<=60)); then dbmtoperc_r=$((67-($dbmtoperc_d-59)))
-    elif ((61<=$dbmtoperc_d && $dbmtoperc_d<=62)); then dbmtoperc_r=$((64-($dbmtoperc_d-61)))
-    elif ((63<=$dbmtoperc_d && $dbmtoperc_d<=64)); then dbmtoperc_r=$((61-($dbmtoperc_d-63)))
-    elif ((65==$dbmtoperc_d)); then dbmtoperc_r=58
-    elif ((66<=$dbmtoperc_d && $dbmtoperc_d<=67)); then dbmtoperc_r=$((56-($dbmtoperc_d-66)))
-    elif ((68==$dbmtoperc_d)); then dbmtoperc_r=53
-    elif ((69==$dbmtoperc_d)); then dbmtoperc_r=51
-    elif ((70<=$dbmtoperc_d && $dbmtoperc_d<=85)); then dbmtoperc_r=$((50-($dbmtoperc_d-70)*2))
-    elif ((86<=$dbmtoperc_d && $dbmtoperc_d<=88)); then dbmtoperc_r=$((17-($dbmtoperc_d-86)*2))
-    elif ((89<=$dbmtoperc_d && $dbmtoperc_d<=91)); then dbmtoperc_r=$((10-($dbmtoperc_d-89)*2))
-    elif ((92==$dbmtoperc_d)); then dbmtoperc_r=3
-    elif ((93<=$dbmtoperc_d)); then dbmtoperc_r=1; fi
-  fi
-  # Retain a (SINGLE_FIELD) here due to [awk '{print $2}'] used below for [$essid] Detection
-  echo "[%$dbmtoperc_r]"
-}
 
 function _get_interface_wifi() {
     local iface
@@ -97,15 +40,11 @@ function _set_interface_wifi() {
 
     if [[ "$state" == "up" ]]; then
         if ! ifup $iface; then
-            echo "Setting Interface $iface $state      "
             ip link set $iface up
-            sleep 5 # Device Busy
         fi
     elif [[ "$state" == "down" ]]; then
         if ! ifdown $iface; then
-            echo "Setting Interface $iface $state      "
             ip link set $iface down
-            sleep 5 # Device Busy
         fi
     fi
 }
@@ -113,7 +52,7 @@ function _set_interface_wifi() {
 function remove_nm_wifi() {
     local iface="$1"
     # delete the NM connection named RetroPie-WiFi
-    nmcli connection delete RetroPie-WiFi 2>&1 | grep -v 'unknown connection'
+    nmcli connection delete RetroPie-WiFi
     _set_interface_wifi $iface down 2>/dev/null
 }
 
@@ -130,16 +69,13 @@ function list_wifi() {
     local iface="$1"
 
     while read line; do
-        [[ "$line" =~ ^Cell && -n "$essid" ]] && echo -e "$quality $essid $frequency\n$type"
-        [[ "$line" =~ ^ESSID ]] && essid=$(echo "$line" | cut -d\" -f2) && if [[ "$essid" == '' ]]; then essid="*"; fi
+        [[ "$line" =~ ^Cell && -n "$essid" ]] && echo -e "$essid\n$type"
+        [[ "$line" =~ ^ESSID ]] && essid=$(echo "$line" | cut -d\" -f2)
         [[ "$line" == "Encryption key:off" ]] && type="open"
         [[ "$line" == "Encryption key:on" ]] && type="wep"
         [[ "$line" =~ ^IE:.*WPA ]] && type="wpa"
-        [[ "$line" =~ ^Frequency ]] && frequency=[$(echo "$line" | awk '{print $1}' | tr -d 'Frequency:' | awk '{ print substr($0, 0, 4) }')GHz]
-        [[ "$line" =~ ^Quality ]] && qualitydBm=$(echo "$line" | awk '{print $3}' | tr -d 'level=')
-        quality="$(dBm_to_percent_B "$qualitydBm")"
-    done < <(iwlist $iface scan | grep -o "Cell .*\|ESSID:\".*\"\|IE: .*WPA\|Encryption key:.*\|Quality.*\|Frequency.*")
-    echo -e "$quality $essid $frequency\n$type"
+    done < <(iwlist $iface scan | grep -o "Cell .*\|ESSID:\".*\"\|IE: .*WPA\|Encryption key:.*")
+    echo -e "$essid\n$type"
 }
 
 function connect_wifi() {
@@ -189,7 +125,6 @@ function connect_wifi() {
         hidden=1
     else
         essid=${essids[choice]}
-        essid=$(echo $essid | awk '{ print $2; }')
         type=${types[choice]}
     fi
 
@@ -345,10 +280,6 @@ function gui_wifi() {
             "3 Will import the SSID (network name) and PSK (password) from the 'wifikeyfile.txt' file on the boot partition
 
 The file should contain two lines as follows\n\nssid = \"YOUR WIFI SSID\"\npsk = \"YOUR PASSWORD\""
-            4 "Enable WiFi Interface"
-            "4 Enable WiFi Interface $iface for this Session"
-            5 "Disable WiFi Interface"
-            "5 Disable WiFi Interface $iface for this Session"
         )
 
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
@@ -387,16 +318,6 @@ The file should contain two lines as follows\n\nssid = \"YOUR WIFI SSID\"\npsk =
                     else
                         printMsgs "dialog" "File 'wifikeyfile.txt' not found on the boot partition!"
                     fi
-                    ;;
-                4)
-                    dialog --defaultno --yesno "This will Enable the WiFi Interface $iface for this Session.\n\nAre you sure you want to continue ?" 12 35 2>&1 >/dev/tty
-                    [[ $? -ne 0 ]] && continue
-                    _set_interface_wifi $iface up 2>/dev/null
-                    ;;
-                5)
-                    dialog --defaultno --yesno "This will Disable the WiFi Interface $iface for this Session.\n\nAre you sure you want to continue ?" 12 35 2>&1 >/dev/tty
-                    [[ $? -ne 0 ]] && continue
-                    _set_interface_wifi $iface down 2>/dev/null
                     ;;
             esac
         else
