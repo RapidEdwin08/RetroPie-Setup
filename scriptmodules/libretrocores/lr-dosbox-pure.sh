52d1ca6977d096caebca14db50190976d320316e
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2026-01-30 08:13:53 -0500

    rpi3 trixie

--- scriptmodules/libretrocores/lr-dosbox-pure.sh
+++ scriptmodules/libretrocores/lr-dosbox-pure.sh
@@ -13,9 +13,22 @@ rp_module_id="lr-dosbox-pure"
 rp_module_desc="DOS emulator"
 rp_module_help="ROM Extensions: .bat .com .cue .dosz .exe .ins .ima .img .iso .m3u .m3u8 .vhd .zip\n\nCopy your DOS games to $ROMDIR/pc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/dosbox-pure/main/LICENSE"
-rp_module_repo="git https://github.com/libretro/dosbox-pure.git main"
+rp_module_repo="git https://github.com/libretro/dosbox-pure.git main :_get_commit_lr-dosbox-pure"
 rp_module_section="exp"
 rp_module_flags=""
+
+function _get_commit_lr-dosbox-pure() {
+    # Pull Latest Commit SHA
+    local branch_tag=main
+    local branch_commit="$(git ls-remote https://github.com/libretro/dosbox-pure.git $branch_tag HEAD | grep $branch_tag | tail -1 | awk '{ print $1}' | cut -c -8)"
+
+    if isPlatform "rpi3"; then
+        #echo 10945cfc; # 20250730 Make 3dfx Voodoo set to "Auto (default)" use hardware acceleration even on OpenGLES devices
+        echo 10823c1e; # 20250729 Set version to 1.0-preview2 - Last Commit Before Issue finding Graphics on rpi3
+    else
+        echo $branch_commit
+    fi
+}
 
 function depends_lr-dosbox-pure() {
     # lr-dosbox-pure will try and use g++ v9 on arm if the system default is v10 due to bugs
