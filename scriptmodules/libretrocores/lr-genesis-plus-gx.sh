7d6009d181ee154e8a54a623c2eee9c91c7538f0
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2026-03-28 04:40:03 -0400

    musicpath [rompath/rombasename] for HW_YX5200

--- scriptmodules/libretrocores/lr-genesis-plus-gx.sh
+++ scriptmodules/libretrocores/lr-genesis-plus-gx.sh
@@ -13,11 +13,24 @@ rp_module_id="lr-genesis-plus-gx"
 rp_module_desc="Sega 8/16 bit emu - Genesis Plus (enhanced) port for libretro"
 rp_module_help="ROM Extensions: .bin .cue .gen .gg .iso .md .sg .smd .sms .zip\nCopy your Game Gear roms to $romdir/gamegear\nMasterSystem roms to $romdir/mastersystem\nMegadrive / Genesis roms to $romdir/megadrive\nSG-1000 roms to $romdir/sg-1000\nSegaCD roms to $romdir/segacd\nThe Sega CD requires the BIOS files bios_CD_U.bin and bios_CD_E.bin and bios_CD_J.bin copied to $biosdir"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/Genesis-Plus-GX/master/LICENSE.txt"
-rp_module_repo="git https://github.com/libretro/Genesis-Plus-GX.git master"
+rp_module_repo="git https://github.com/libretro/Genesis-Plus-GX.git master :_get_commit_lr-genesis-plus-gx"
 rp_module_section="main"
+
+function _get_commit_lr-genesis-plus-gx() {
+    # Pull Latest Commit SHA - Allow RP Module Script to Check against Latest Source
+    local branch_tag=master
+    local branch_commit="$(git ls-remote https://github.com/libretro/Genesis-Plus-GX.git $branch_tag HEAD | grep $branch_tag  | tail -1 | awk '{ print $1}' | cut -c -8)"
+
+    #echo d446078a # 20260306
+    echo $branch_commit
+}
 
 function sources_lr-genesis-plus-gx() {
     gitPullOrClone
+    if [[ "$(_get_commit_lr-genesis-plus-gx)" == "d446078a" ]]; then
+        applyPatch "$md_data/death_and_lead_d446078a.diff" # 033df9b7
+        applyPatch "$md_data/yx5200_musicpath_d446078a.diff" # musicpath [rompath/rombasename] for HW_YX5200
+    fi
 }
 
 function build_lr-genesis-plus-gx() {
