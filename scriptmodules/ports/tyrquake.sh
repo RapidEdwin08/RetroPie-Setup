8be5d1078010985dcd4793e7c5df4f6f3417e30b
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2026-04-07 20:41:23 -0400

    replace %QUAKEDIR% with %ROM% +params

--- scriptmodules/ports/tyrquake.sh
+++ scriptmodules/ports/tyrquake.sh
@@ -48,17 +48,42 @@ function install_tyrquake() {
     )
 }
 
+function _add_games_tyrquake() {
+    local cmd="$1"
+    declare -A games=(
+        ['id1']="Quake"
+        ['hipnotic']="Quake I Mission Pack 1 (hipnotic)"
+        ['rogue']="Quake I Mission Pack 2 (rogue)"
+        ['dopa']="Quake I Episode 5 (dopa)"
+    )
+    local dir
+    local pak
+    for dir in "${!games[@]}"; do
+        pak="$romdir/ports/quake/$dir/pak0.pak"
+        if [[ -f "$pak" ]]; then
+            addPort "$md_id" "quake" "${games[$dir]}" "$cmd" "$pak"
+        fi
+    done
+
+    if [[ ! "$(ls -1 "$romdir/ports/quake/honey" 2>/dev/null)" == '' ]]; then
+        addPort "$md_id" "quake" "Quake I AddOn Honey" "$binary ${params[*]}" "$romdir/ports/quake/honey/pak0.pak"
+    fi
+}
+
 function add_games_tyrquake() {
-    local params=("-basedir $romdir/ports/quake" "-game %QUAKEDIR%")
-    local binary="$md_inst/bin/tyr-quake"
+    ##local params=("-basedir $romdir/ports/quake")
+    ##params+=("-game %QUAKEDIR%")
+    ##local binary="$md_inst/bin/tyr-quake"
+    local params=("%ROM%")
+    local binary="$md_inst/tyrquake.sh"
 
     isPlatform "kms" && params+=("-width %XRES%" "-height %YRES%")
     if ( isPlatform "kms" || isPlatform "mesa" ) || ( isPlatform "gl" || isPlatform "vulkan" ); then
         params+=("+set vid_vsync 2")
-        binary="$md_inst/bin/tyr-glquake"
+        ##binary="$md_inst/bin/tyr-glquake"
     fi
 
-    _add_games_lr-tyrquake "$binary ${params[*]}"
+    _add_games_tyrquake "$binary ${params[*]}"
 }
 
 function configure_tyrquake() {
@@ -69,4 +94,31 @@ function configure_tyrquake() {
     add_games_tyrquake
 
     moveConfigDir "$home/.tyrquake" "$md_conf_root/quake/tyrquake"
+
+    local binary="$md_inst/bin/tyr-quake"
+    if ( isPlatform "kms" || isPlatform "mesa" ) || ( isPlatform "gl" || isPlatform "vulkan" ); then binary="$md_inst/bin/tyr-glquake"; fi
+    cat >"$md_inst/tyrquake.sh" << _EOF_
+#!/bin/bash
+
+# get Params: remove everything up to pak - [+map dm7sp]
+quake_params="\${@##*pak}"
+
+# get basedir: remove everything up to /quake/ - [dm7sp/pak0.pak +map dm7sp]
+quake_dir="\${1##*/quake/}"
+
+# get basedir: remove filename - [dm7sp]
+quake_dir="\${quake_dir%/*}"
+
+# Logging
+echo -basedir: [\$quake_dir] +Params: [\$quake_params] >> /dev/shm/runcommand.log
+
+# Called by [emulators.cfg] with %ROM% instead of %QUAKEDIR%
+if [[ "\$quake_dir" == 'id1' ]]; then # [-game id1] fails to play Shareware Episode
+    $binary -basedir $romdir/ports/quake
+else
+    $binary -basedir $romdir/ports/quake -game \$quake_dir \$quake_params
+fi
+
+_EOF_
+    chmod 755 "$md_inst/tyrquake.sh"
 }
