0e558aef9ae7679cb62744856cee39ff37ebe3b2
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2026-06-25 19:50:10 -0400

    yx5200_musicpath_173bf8af

--- scriptmodules/libretrocores/lr-genesis-plus-gx.sh
+++ scriptmodules/libretrocores/lr-genesis-plus-gx.sh
@@ -22,15 +22,16 @@ function _get_commit_lr-genesis-plus-gx() {
     local branch_commit="$(git ls-remote https://github.com/libretro/Genesis-Plus-GX.git $branch_tag HEAD | grep $branch_tag  | tail -1 | awk '{ print $1}' | cut -c -8)"
 
     #echo ee71e47c; #20260331 add linux-aarch64 build (#401)
-    #echo 162c3439; #20260515 Fetch translations & Recreate libretro_core_options_intl.h
+    #echo f33876ca; #20260619 Fetch translations & Recreate libretro_core_options_intl.h
+    #echo 173bf8af; #20260625 Merge branch 'master' of https://github.com/ekeeke/Genesis-Plus-GX into rebase
     echo $branch_commit
 }
 
 function sources_lr-genesis-plus-gx() {
     gitPullOrClone
-    if [[ "$(_get_commit_lr-genesis-plus-gx)" == "162c3439" ]]; then
-        applyPatch "$md_data/death_and_lead_ee71e47c.diff" # 033df9b7
-        applyPatch "$md_data/yx5200_musicpath_ee71e47c.diff" # musicpath [rompath/rombasename] for HW_YX5200
+    if [[ "$(_get_commit_lr-genesis-plus-gx)" == "173bf8af" ]]; then
+        ##applyPatch "$md_data/death_and_lead_ee71e47c.diff" # 033df9b7 ### Now Included since 173bf8af ###
+        applyPatch "$md_data/yx5200_musicpath_173bf8af.diff" # musicpath [rompath/rombasename] for HW_YX5200
     fi
 }
 
