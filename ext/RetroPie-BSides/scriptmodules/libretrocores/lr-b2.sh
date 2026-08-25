6a5624233745b7c39dcb7563e82207fccece00c1
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2026-08-25 07:58:52 -0400

    Remove update ~/.emulationstation/es_systems.cfg
    creates empty es_systems.cfg when no pre-existing file found.
    not really needed

--- ext/RetroPie-BSides/scriptmodules/libretrocores/lr-b2.sh
+++ ext/RetroPie-BSides/scriptmodules/libretrocores/lr-b2.sh
@@ -41,7 +41,7 @@
 
 rp_module_id="lr-b2"
 rp_module_desc="BBC Micro emulator: Libretro core of the b2 emulator for RetroArch"
-rp_module_help="ROM Extension: .dsd .ssd .zip\n\nCopy your roms to $romdir/bbcmicro"
+rp_module_help="ROM Extension: .dsd .ssd .zip\n\nCopy your roms to $romdir/bbcmicro\n\nRetroPie scriptmodule SOURCE:\n\nhttps://github.com/Gemba/RetroPie-BSides.git\n\nhttps://github.com/Gemba/RetroPie-BSides/blob/main/docs/Libretro_bbcmicro.md"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/zoltanvb/b2-libretro/refs/heads/master/src/COPYING"
 rp_module_repo="git https://github.com/zoltanvb/b2-libretro.git master"
 rp_module_section="exp"
@@ -90,58 +90,6 @@ function configure_lr-b2() {
             sed -i "/^input_remapping_directory =/a input_auto_game_focus = \"1\"" "$fn"
 
         mkRomDir "$system_name"
-        # update ~/.emulationstation/es_systems.cfg
-        local xpath="/systemList/system[name='$system_name']"
-        local system_bbc_es_cfg="$(xmlstarlet select \
-            --template \
-            --copy-of "$xpath" \
-            "/etc/emulationstation/es_systems.cfg")"
-
-        local user_es_cfg="$home/.emulationstation/es_systems.cfg"
-        # if not there add system from global es_systems.cfg
-        if [[ $(xmlstarlet select \
-            --template \
-            -v "count($xpath)" \
-            "$user_es_cfg") -eq 0 ]]; then
-            sed -i "/<\/systemList>/d" "$user_es_cfg"
-            printf "%s" "$system_bbc_es_cfg" >>"$user_es_cfg"
-            printf "%s" "</systemList>" >>"$user_es_cfg"
-        fi
-
-        # set system fullname if not present
-        [[ -z "$(xmlstarlet select \
-            --template \
-            --copy-of "$xpath/fullname/text()" "$user_es_cfg" | xargs)" ]] &&
-            xmlstarlet edit \
-                --inplace \
-                --update "$xpath/fullname" \
-                --value "BBC Microcomputer System" "$user_es_cfg"
-
-        # check mandatory extensions present
-        local current_exts=$(xmlstarlet select \
-            --template \
-            --copy-of "$xpath/extension/text()" \
-            "$user_es_cfg" | xargs)
-
-        for ext in ".dsd" ".ssd"; do
-            if [[ "$current_exts" != *"$ext"* ]]; then
-                current_exts=$(printf "%s" "$current_exts $ext" | xargs)
-                xmlstarlet edit \
-                    --inplace \
-                    --update "$xpath/extension" \
-                    --value "$current_exts" \
-                    "$user_es_cfg"
-            fi
-        done
-
-        # sort user's es_systems.cfg
-        local tmp_piggy=$(mktemp)
-        cp "$user_es_cfg" "$tmp_piggy"
-        xmlstarlet select --xml-decl --indent \
-            --template --match "/" --elem "systemList" \
-            --match "//system" --sort A:T:U "name" --copy-of "." \
-            "$tmp_piggy" >"$user_es_cfg"
-        rm "$tmp_piggy"
     fi
 
     addEmulator 0 "$md_id" "$system_name" "$md_inst/b2_libretro.so"
